507d7736f1cdab60861dd7703d29edb5a4bc89d7
Gilyerm <gilyerm@gmail.com>
2020-01-28 19:30:29 +0200

    re-route to index

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var logger = require('morgan');
 var cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testApiRouter = require('./routes/testApi');
 
 var app = express();
 
@@ -23,8 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/testApi',testApiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
+const inside = require('point-in-polygon');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Endpoint1 - query all of the vehicle locations
+router.get("/",async function(req,res,next){
+  console.log(req.query);
+  let json = await fetch("https://raw.githubusercontent.com/Autofleet/code-challenge-vehicle-map/master/vehicles-location.json")
+      .then(res => res.json());
+  res.send(json);
+});
+
+// Endpoint 2- get vehicles ids that are inside of the specific polygon
+router.get("/query/",async function (req, res, next) {
+  const polygon = JSON.parse(req.query.data);
+  let json = await fetch("https://raw.githubusercontent.com/Autofleet/code-challenge-vehicle-map/master/vehicles-location.json")
+      .then(res => res.json());
+  json = json.filter((v)=> inside([v.location.lat,v.location.lng],polygon));
+  res.send(json);
 });
 
 
