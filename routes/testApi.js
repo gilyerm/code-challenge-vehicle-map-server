b80759d78ab064090e0bb4eb16dad61824e45728
Gilyerm <gilyerm@gmail.com>
2020-01-28 17:34:50 +0200

    get all in polygon

--- routes/testApi.js
+++ routes/testApi.js
@@ -1,11 +1,28 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 const fetch = require('node-fetch');
+const inside = require('point-in-polygon');
+
 
 router.get("/",async function(req,res,next){
+    console.log(req.query);
+
     let json = await fetch("https://raw.githubusercontent.com/Autofleet/code-challenge-vehicle-map/master/vehicles-location.json")
         .then(res => res.json());
-    res.send(JSON.stringify(json));
+    res.send(json);
 });
+
+router.get("/query/",async function (req, res, next) {
+   // console.log(req.query);
+   const polygon = JSON.parse(req.query.data);
+   // console.log(JSON.parse(req.query.data));
+
+    let json = await fetch("https://raw.githubusercontent.com/Autofleet/code-challenge-vehicle-map/master/vehicles-location.json")
+        .then(res => res.json());
+    json = json.filter((v)=> inside([v.location.lat,v.location.lng],polygon));
+    // console.log(json);
+    res.send(json);
+});
+
 
 module.exports = router;
